1b5d204ede6433e80ccbcc4efedba11d23b53c5e
Brandon Leitch <brandonleitch99@gmail.com>
2018-01-11 12:29:29 -0500

    Complete 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,26 @@
 #write your code here
+def add(a,b)
+  a + b
+end
+
+def subtract(a,b)
+  a - b
+end
+
+def sum(arr)
+  arr.inject(0) {|sum, n| add(sum, n)}
+end
+
+def multiply(arr)
+  arr.reduce(1, :*)
+end
+
+def power(a,b)
+  if b == 0 then 1
+  else a * power(a, b-1) end
+end
+
+def factorial(n)
+  if n < 2 then 1
+  else n * factorial(n - 1) end
+end
