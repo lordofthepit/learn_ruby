6575c5ff23121844f60003bf6522801a9113f54d
Brandon Leitch <brandonleitch99@gmail.com>
2018-01-12 11:55:39 -0500

    Complete 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
 # write your code here
+  @@little = ["the","a","an","and","in","of"]
+
+  attr_reader :title
+
+  def title=(phrase)
+    @title = phrase.split(" ").map.with_index{|word, i|
+      if i == 0 then word.capitalize
+      elsif @@little.include? word then word
+      else word.capitalize end
+    }.join(" ")
+  end
 end
