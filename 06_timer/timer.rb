52bbe9da2c8fc25b4409bdb6af449ac077cc271a
Brandon Leitch <brandonleitch99@gmail.com>
2018-01-12 12:37:17 -0500

    Complete 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    seconds = @seconds % 60
+    minutes = @seconds / 60
+
+    hours = minutes / 60
+    minutes = minutes % 60
+
+    "#{pad(hours)}:#{pad(minutes)}:#{pad(seconds)}"
+  end
+
+  def pad(n)
+    if n.to_s.length == 1 then "0#{n}"
+    else n.to_s end
+  end
 end
