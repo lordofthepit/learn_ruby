eca1475297618b99d694cb0d7f3860288730d8de
Brandon Leitch <brandonleitch99@gmail.com>
2018-01-12 11:38:44 -0500

    Complete 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,12 @@
 #write your code here
+def translate(phrase)
+  phrase.split(" ").map { |e| translate_word(e) }.join(" ")
+end
+
+$regexp = /^(qu|(?=[a-z])[^aeiou])+/
+
+def translate_word(str)
+  match = str[$regexp];
+  if match != nil then str.sub!(match,"").concat(match) end
+  "#{str}ay"
+end
