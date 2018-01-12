29138d39cbf185efe5dfe6f9cbddfb789e1f4241
Brandon Leitch <brandonleitch99@gmail.com>
2018-01-12 11:20:08 -0500

    Complete 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
 #write your code here
+def echo(str)
+  str
+end
+
+def shout(str)
+  str.upcase
+end
+
+def repeat(str, n = 2)
+  ("#{str} " * n).chomp(" ")
+end
+
+def start_of_word(str, n = 1)
+  str[0, n]
+end
+
+def first_word(str)
+  str.split(" ")[0]
+end
+
+def titleize(str)
+  str.split(" ").map.with_index { |e, i|
+    if i == 0 then e.capitalize
+    elsif  ["and","over","the"].include? e then e
+    else e.capitalize end
+  }.join(" ");
+end
